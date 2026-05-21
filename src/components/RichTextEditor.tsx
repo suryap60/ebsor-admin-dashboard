383c8aa7eeb72341624f7664ed5cedf3fd496656
surya <suryap@ebsor.com>
2026-05-21 16:16:16 +0530

    fix: text editor updated

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -4,7 +4,9 @@ import dynamic from "next/dynamic";
 import "react-quill-new/dist/quill.snow.css";
 import { useMemo } from "react";
 
-const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+const ReactQuill = dynamic(() => import("react-quill-new"), {
+  ssr: false,
+});
 
 interface RichTextEditorProps {
   value: string;
@@ -12,29 +14,69 @@ interface RichTextEditorProps {
   placeholder?: string;
 }
 
-export default function RichTextEditor({ value, onChange, placeholder }: RichTextEditorProps) {
+export default function RichTextEditor({
+  value,
+  onChange,
+  placeholder,
+}: RichTextEditorProps) {
   const modules = useMemo(
     () => ({
       toolbar: [
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ header: [1, 2, 3, false] }],
+        [{ font: [] }],
+        [{ size: ["small", false, "large", "huge"] }],
+
         ["bold", "italic", "underline", "strike"],
+
+        [{ color: [] }, { background: [] }],
+
+        [{ script: "sub" }, { script: "super" }],
+
         [{ list: "ordered" }, { list: "bullet" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+
+        [{ align: [] }],
+
+        ["blockquote", "code-block"],
+
         ["link", "image"],
+
         ["clean"],
       ],
     }),
     []
   );
 
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "color",
+    "background",
+    "script",
+    "list",
+    "indent",
+    "align",
+    "blockquote",
+    "code-block",
+    "link",
+    "image",
+  ];
+
   return (
-    <div className="bg-white text-black rounded-md overflow-hidden">
+    <div className="bg-white dark:bg-zinc-950 rounded-xl overflow-hidden">
       <ReactQuill
         theme="snow"
         value={value}
         onChange={onChange}
         modules={modules}
+        formats={formats}
         placeholder={placeholder}
-        className="h-64 mb-12"
+        className="h-64 mb-12 text-black dark:text-white"
       />
     </div>
   );
